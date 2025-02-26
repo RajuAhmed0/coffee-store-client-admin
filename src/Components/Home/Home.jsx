dcd369ddd7fa9f389b7601de7197522da2811623
RajuAhmed0 <itzraju98@gmail.com>
2025-02-26 14:54:10 +0600

    footer section com

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Buttom_Banner from './Banner/Buttom_Banner';
+import Our_Products from './Our_Popular_Products/Our_Products';
 
 const Home = () => {
     return (
         <div>
           <Banner></Banner>
           <Buttom_Banner></Buttom_Banner>
+          <Our_Products></Our_Products>
         </div>
     );
 };
--- src/Components/Home/Our_Popular_Products/Our_Products.jsx
+++ src/Components/Home/Our_Popular_Products/Our_Products.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { RiCupLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const Our_Products = () => {
     return (
         <div>
-            Our_Products
-            Our_Products
+            <div className="text-center pb-10 md:mt-[120px] mt-16">
+                <p className="railwayFont font-normal text-xl text-[#1B1A1A] mb-2">
+                    --- Sip & Savor ---
+                </p>
+                <h3 className="text-[#331A15] md:text-[55px] text-4xl font-normal md:mb-4 mb-2">
+                    Our Popular Products
+                </h3>
+                <Link
+                    to="/add-coffee"
+                    className="btn rounded-none mt-4 border-2 border-[#331A15] hover:text-black bg-[#E3B577] hover:bg-[#e3b477ef] text-2xl font-normal"
+                >
+                    <span className="text-white ">Add Coffee</span>
+                    <RiCupLine className="text-chocolate" />
+                </Link>
+
+
+            </div>
         </div>
     );
 };

2b8f1c433afbb74c246aa4676796932266f20804
RajuAhmed0 <itzraju98@gmail.com>
2025-02-26 12:14:23 +0600

    banner section com

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className=''>
-            <div
-                className="max-w-[1920px] mx-auto h-[800px] bg-[url('https://i.ibb.co.com/KphNY33r/3.png')] bg-cover"
-               >
-               
-                <div className="pt-[288px] ml-[889px] mr-[335px]">
-                    <div className="text-left">
-                        <h1 className="text-white text-[55px]">
-                            Would you like a Cup of Delicious Coffee?
-                        </h1>
-                        <p className="text-white  md:text-base text-sm railwayFont mb-8 mt-4">
-                            It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back! Your companion of every moment!!!
-                            Enjoy the beautiful moments and make them memorable.
-                        </p>
-                        <button className=" px-[22px] py-[9px] bg-[#E3B577] text-[#242222] text-2xl   hover:bg-[#d19c69] transition">
-                            Learn More
-                        </button>
-                    </div>
+        <div
+            className="hero min-h-screen text-center sm:text-left"
+            style={{
+                backgroundImage: "url(https://i.ibb.co.com/KphNY33r/3.png)",
+            }}
+        >
+            <div className="container mx-auto px-3 md:px-6 grid grid-cols-1 md:grid-cols-2 text-white">
+                <div className="hidden md:block select-none"></div>
+                <div className="px-4 md:px-0">
+                    <h1 className="mb-5 2xl:text-[55px] xl:text-5xl md:text-4xl text-3xl  font-bold">
+                        Would you like a Cup of Delicious Coffee?
+                    </h1>
+                    <p className="md:leading-[30px] mb-5 railwayFont font-normal md:text-base text-sm">
+                        It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.
+                    </p>
+                    <button className=" bg-[#E3B577] px-[22px] py-[9px] font-normal text-2xl text-[#242222]  hover:bg-[#e3b477de] hover:text-white">
+                        Learn More
+                    </button>
                 </div>
             </div>
         </div>
